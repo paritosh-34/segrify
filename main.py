f1c24367334b6f5a18830d6e8f9b22680a6b1fad
Nakul garg <nakulgarg2000@gmail.com>
2020-05-06 20:33:25 +0530

    Check main.py

--- main.py
+++ main.py
@@ -7,9 +7,20 @@
 # print(os.getcwd())
 import tkinter as tk
 from tkinter import filedialog
+import time
+import os
 
 root = tk.Tk()
 root.withdraw()
 
-file_path = filedialog.askdirectory()
+file_path = filedialog.askopenfilename()
 print(file_path)
+__file__ = file_path
+
+
+print('File         :', __file__)
+print('Access time  :', time.ctime(os.path.getatime(__file__)))
+print('Modified time:', time.ctime(os.path.getmtime(__file__)))
+print('Change time  :', time.ctime(os.path.getctime(__file__)))
+print('Size         :', os.path.getsize(__file__))
+
